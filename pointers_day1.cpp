2ec578e8bba606c9d187edecc74d33fc747800b1
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-23 21:13:17 +0530

    learn about point in array

---
+++ pointers_day1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a = 9 ;
+    int b = 10 ;
+    // cout << a << endl;
+    cout << &a << endl;
+    cout << &b << endl;
+    return 0;
+}
