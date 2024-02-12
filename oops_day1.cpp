9edf29cc4a7212e599ffe864016acffb2174137c
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-12 18:35:15 +0530

    created setter in object

--- oops_day1.cpp
+++ oops_day1.cpp
@@ -1,30 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Student
-{   
-    public:
+{
+private:
     string name;
     int rollNo;
     int age;
     string grade;
-    // setter 
-    void setName (string name){
-        this->name = name; 
+    // setter
+public:
+    void setName(string name)
+    {
+        this->name = name;
     }
-    void setRollNo(int rollNo){
+    void setRollNo(int rollNo)
+    {
         this->rollNo = rollNo;
     }
-    void setAge(int age){
+    void setAge(int age)
+    {
         this->age = age;
     }
-    void setGrade(string grade){
+    void setGrade(string grade)
+    {
         this->grade = grade;
     }
 };
 int main()
 {
     Student s1;
-    s1.age = 0 ;
-    cout << s1.age << endl;
-     return 0;
+    s1.setAge(12);
+    s1.setGrade("A+");
+    s1.setName("Bharat ruidas");
+    return 0;
 }
