a608134749ca32f984b2f6d9422447be403f0465
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-16 21:31:39 +0530

    day 2 leetcode challenge completed

--- oops_day_2_lec_2.cpp
+++ oops_day_2_lec_2.cpp
@@ -3,40 +3,82 @@
 //
 #include <bits/stdc++.h>
 using  namespace  std;
-class Customer {
-    string name ;
-    int balance , account_number ;
-public:
-    Customer(string name , int balance , int account_number){
-        this->name = name ;
-        this->account_number = account_number ;
-        this->balance = balance;
-    }
-    // deposit
-    void deposit (int amount){
+//Given a string paragraph and a string array of the banned words banned,
+//return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned,
+//and that the answer is unique.
 
-        if (amount > 0 ){
-            cout << "success fully deposit" << endl;
-        }else{
-            throw "please give right amount ." ;
+//The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+//Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+//Output: "ball"
+string mostCommonWord(string paragraph, vector<string>& banned) {
+    string initial = "";
+    transform(paragraph.begin() , paragraph.end() , paragraph.begin() , ::tolower);
+    vector<string > all_string ;
+    for (int i = 0  ; i < paragraph.length() ; i++){
+        if (isalpha(paragraph[i])){
+            initial += paragraph[i] ;
+        }else {
+            all_string.push_back(initial);
+            initial = "";
         }
     }
-    // withdraw
-    void withdraw (int amount ){
-        if (this->balance >= amount){
-            cout << "successfully withdraw" << endl;
-        }else{
-            throw "Not have enough money in balance ." ;
+//    if(initial[initial.length()-1] == '.')
+//    initial.pop_back();
+    all_string.push_back(initial);
+    map<string , int >map_of_string;
+    for (int i = 0 ; i < all_string.size() ; i++){
+        map_of_string[all_string[i]]++;
+    }
+    set<string>s ;
+    for (auto x : banned){
+        transform(x.begin() , x.end() , x.begin() , ::tolower);
+        s.insert(x);
+    }
+    string ans = "" ;
+    int len = INT_MIN ;
+    for (auto x : map_of_string){
+        if (s.find(x.first) == s.end()){
+            if (x.first.length() != 0){
+                if (x.second > len){
+                    ans = x.first ;
+                    len = x.second ;
+                }
+            }
         }
     }
-};
+    return ans;
+}
+//class Customer {
+//    string name ;
+//    int balance , account_number ;
+//public:
+//    Customer(string name , int balance , int account_number){
+//        this->name = name ;
+//        this->account_number = account_number ;
+//        this->balance = balance;
+//    }
+//    // deposit
+//    void deposit (int amount){
+//
+//        if (amount > 0 ){
+//            cout << "success fully deposit" << endl;
+//        }else{
+//            throw "please give right amount ." ;
+//        }
+//    }
+//    // withdraw
+//    void withdraw (int amount ){
+//        if (this->balance >= amount){
+//            cout << "successfully withdraw" << endl;
+//        }else{
+//            throw "Not have enough money in balance ." ;
+//        }
+//    }
+//};
 int main (){
-    Customer c1 ("bharat" , 5000 , 1234);
-    try {
-            c1.deposit(-2025) ;
-//        c1.withdraw(6000) ;
-    }catch (const char* e ){
-        cout << "Exception occured : " << e << endl;
-    }
-    return 0 ;
+
+   string  paragraph = "Bob hit a ball, the hit BALL flew far after it was hit." ;
+   vector<string>banned{ "hit"};
+    cout << mostCommonWord(paragraph , banned)  << endl;
+
  }
