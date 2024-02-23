d3d9806414843717961a717f20f9485d6bc63181
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-02-23 23:35:35 +0530

    day 9 completed

---
+++ day_9_binary_search_lecture_1.cpp
@@ -0,0 +1,22 @@
+//
+// Created by bharat on 23-02-2024.
+//
+#include <bits/stdc++.h>
+using namespace  std;
+int binarySearch(vector<int>&arr , int x ){
+    int low = 0 , high = arr.size()-1;
+    while (low <=  high ) {
+        int mid = low + (high - low)/2 ;
+        if (arr[mid] == x ) return mid ;
+        else if (arr[mid] > x ) high = mid -1 ;
+        else
+            low = mid +1;
+    }
+    return -1;
+}
+int main (){
+    vector<int>arr {1,2,3,4};
+    int x = 4  ;
+    cout << binarySearch(arr , x ) ;
+    return 0 ;
+}
