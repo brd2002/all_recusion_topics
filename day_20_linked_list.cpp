0beeddeb86260701dfd94c6fcbb2589e0ae8b052
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-05 12:27:04 +0530

    this is the first lecture

--- day_20_linked_list.cpp
+++ day_20_linked_list.cpp
@@ -4,6 +4,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 class Node {
+public:
     int data ;
     Node * next ;
     Node (int value){
@@ -11,7 +12,21 @@ class Node {
         next = NULL;
     }
 };
+Node * createLinkedList(vector<int>&arr , int index , Node * prev ){
+    if (index == arr.size()) return prev ;
+    Node *temp = new Node(arr[index]);
+    temp->next = prev;
+    return createLinkedList(arr , index+1 , temp);
+}
 int main (){
+    vector<int>arr {1,2,3,4,5};
+    Node * head ;
+    head = createLinkedList(arr , 0 , NULL);
+    Node * temp = head;
+    while (temp!= NULL){
+        cout << temp->data << endl;
+        temp = temp->next ;
+    }
 
     return 0 ;
 }
