9a0fd2d4ec98441890bedd47a77cb8fe63a351a8
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-05 13:02:30 +0530

    lecture 1 completed

--- day_20_linked_list.cpp
+++ day_20_linked_list.cpp
@@ -18,10 +18,23 @@ Node * createLinkedList(vector<int>&arr , int index , Node * prev ){
     temp->next = prev;
     return createLinkedList(arr , index+1 , temp);
 }
+Node * insertGivenPosition(Node * head , int val , int pos){
+    Node * temp = head  ;
+    --pos;
+    while (--pos){
+        temp = temp->next;
+    }
+    Node * temp2 = new Node(val);
+    temp2->next = temp->next ;
+    temp->next = temp2;
+    return head ;
+}
 int main (){
     vector<int>arr {1,2,3,4,5};
     Node * head ;
     head = createLinkedList(arr , 0 , NULL);
+    head = insertGivenPosition(head , 10 , 2);
+    head = insertGivenPosition(head , 30 , 3);
     Node * temp = head;
     while (temp!= NULL){
         cout << temp->data << endl;
