ac9a453eb2370ab7b815319bfd811def8ec6ff31
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-05 11:36:20 +0530

    this is the first commit of day 20

--- day_19_linkedlist.cpp
+++ day_19_linkedlist.cpp
@@ -3,6 +3,55 @@
 //
 #include <bits/stdc++.h>
 using namespace std;
+class Node {
+public:
+    int data ;
+    Node * next ;
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+// insert node using recursion
+Node * insertNode (vector<int>&arr , int index ){
+    if (index == arr.size()){
+        return NULL;
+    }
+    Node * temp ;
+    temp = new Node (arr[index]);
+    temp->next = insertNode(arr , index+1);
+    return temp;
+}
 int main (){
-
+    Node * head = NULL ;
+//    head = new Node(4);
+//Node *tail = NULL;
+vector<int>arr{1,2,3,4,5};
+head = insertNode(arr , 0) ;
+//    for (int i = 0 ; i < arr.size() ; i++){
+//        if (head == NULL){
+//            head = new Node(arr[i]);
+//
+//        }else {
+//            Node * temp = new Node (arr[i]);
+//            temp->next = head ;
+//            head = temp ;
+//        }
+//    }
+//    for (int i = 0 ; i < arr.size() ; i++){
+//        if (head == NULL){
+//            head = new Node(arr[i]);
+//            tail = head;
+//
+//        }else {
+//            tail->next = new Node(arr[i]);
+//            tail = tail->next;
+//        }
+//    }
+    Node * temp = head;
+    while (temp != NULL){
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+    return 0 ;
 }
---
+++ day_20_linked_list.cpp
@@ -0,0 +1,17 @@
+//
+// Created by bharat on 05-03-2024.
+//
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+    int data ;
+    Node * next ;
+    Node (int value){
+        data = value ;
+        next = NULL;
+    }
+};
+int main (){
+
+    return 0 ;
+}
