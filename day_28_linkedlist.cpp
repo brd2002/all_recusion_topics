18bea638d2d48b629ff75c0fb1740a4b6f97b21b
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-13 22:50:58 +0530

    solved one problem or confusion about string value by reference or value by

---
+++ day_28_linkedlist.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *next;
+    Node (int value){
+        data = value;
+        next = NULL;
+    }
+};
+// Node* reverseList(struct Node *head)
+//     {
+//         // code here
+//         // return head of reversed list
+//         // 1 2 3 4 5 6 
+//         // prev , curr 
+//         Node *prev = NULL;
+//         Node *curr = head;
+//         while(curr){
+//             curr->next = prev;
+
+//         }
+//     }
+    
+// int giveLongestPrefixSum (string &s){
+    
+// }
+// void changestring(string &s){
+//     swap(s[0] , s[s.length()-1]);
+// }
+int main()
+{   
+    // string s = "bharat";
+    // changestring(s);
+    // cout << s << endl;
+
+    return 0;
+}
