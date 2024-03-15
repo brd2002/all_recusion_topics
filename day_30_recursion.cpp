122f343a46a71ba069592101ff06d42cbbb56778
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-15 21:33:40 +0530

    second program is completed

--- day_30_recursion.cpp
+++ day_30_recursion.cpp
@@ -1,6 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+void fun(int days){
+    if (days == 0 ){
+        cout << "Happy birthday" << endl;
+        return ;
+    }
+    cout << days << " days are left for birthday." << endl;
+    fun (days-1) ;
+}
+// print all even numbers
+void printEvenUsingRecursion(int numbers){
+    if (numbers == 0){
+        cout << "even number " << numbers << endl;
+        return;
+    }
+    printEvenUsingRecursion(numbers-1);
+    if (numbers%2 == 0){
+        cout << "even number " << numbers << endl;
+    }
+}
+int main()
+{
+    // fun(10);
+    printEvenUsingRecursion(10) ;
     return 0;
 }
