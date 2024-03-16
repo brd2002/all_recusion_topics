3770d1ef11abbe1b2aa1e725f7e85edb0ac0cdd2
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-16 19:18:15 +0530

    gcd completed

--- recursion.cpp
+++ recursion.cpp
@@ -5,7 +5,19 @@ int fibonacci(int n){
     else 
     return fibonacci(n-1)+fibonacci(n-2);
 }
+int nthstair(int n){
+    if (n == 1 ) return 1;
+    if (n == 2) return 2;
+    return nthstair(n-1)+nthstair(n-2);
+}
+int gcd (int a, int b){
+    if (b == 0 ){
+        return a ;
+    }
+    return gcd(b , a%b) ;
+}
 int main (){
-    int result = fibonacci(1);
-    cout << result << endl;
+    // int result = fibonacci(1);
+    // cout << result << endl;
+    cout << gcd(10 , 4) << endl;
 }
