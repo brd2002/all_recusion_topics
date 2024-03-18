90921c03a559d2f4c39473ef42a9d19e8e05b699
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-18 14:54:05 +0530

    two problems solved

--- recursion.cpp
+++ recursion.cpp
@@ -25,16 +25,39 @@ int gcd(int a, int b)
 }
 int maxelement(vector<int> &arr, int index)
 {
-    if (index == arr.size()-1){
+    if (index == arr.size() - 1)
+    {
         return arr[index];
     }
     return max(arr[index], maxelement(arr, index + 1));
+}
+bool checkPalindrom(string &s, int start, int end)
+{
+    if (start >= end)
+        return true;
+    else if (s[start] != s[end])
+        return false;
+    return checkPalindrom(s, start + 1, end - 1);
+}
+int countVowel(string &s, int index)
+{
+    if (index == s.length()) return 0 ;
+    if (s[index] == 'a' || s[index] == 'e' || s[index] == 'i' || s[index] == 'o' || s[index] == 'u')
+    {
+        return 1 + countVowel(s, index + 1);
+    }
+    else {
+        return countVowel(s , index+1);
+    }
 }
 int main()
 {
     // int result = fibonacci(1);
     // cout << result << endl;
     // cout << gcd(10, 4) << endl;
-    vector<int>arr {1,4,3,4,9,8,10};
-    cout << maxelement(arr , 0);
+    // vector<int>arr {1,4,3,4,9,8,10};
+    // cout << maxelement(arr , 0);
+    string a = "bharat ruidas";
+    // cout << checkPalindrom(a, 0, a.length() - 1);
+    cout << countVowel(a , 0) << endl;
 }
